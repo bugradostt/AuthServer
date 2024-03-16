51137d68ed330e76e5334bcd5701506efbce9962
bugradostt <bugradostt@gmail.com>
2024-03-16 12:30:17 +0300

    entity add

---
+++ AuthServer.Core/Entity/UserRefreshToken.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Entity
+{
+    public class UserRefreshToken
+    {
+        public string UserId { get; set; }
+        public string Code { get; set; } // token code
+        public DateTime Expiration { get; set; } // tokenin ömrü
+    }
+}
