4ea2c09e332b8d735425c891b713b70f0773c8aa
bugradostt <bugradostt@gmail.com>
2024-03-17 22:55:44 +0300

    add interface repository

---
+++ SharedLibrary/Dtos/ErrorDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Dtos
+{
+    public class ErrorDto
+    {
+        public List<string> Errors { get; set; }
+
+        public bool IsShow { get; set; }
+    }
+}
